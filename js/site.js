37bc84edf62226eddd12624b8c202994dec4be76
wiebkem <wickoby@gmail.com>
2017-12-17 18:18:02 +0100

    new initial website

--- js/site.js
+++ js/site.js
@@ -8,21 +8,25 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top)
+          scrollTop: (target.offset().top - 55)
         }, 1000, "easeInOutExpo");
         return false;
       }
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#sideNav'
+    target: '#navigation',
+    offset: 57
+  });
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $(".navbar").addClass("d-none");
+  });
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $(".navbar").removeClass("d-none");
   });
 
 })(jQuery); // End of use strict
