71feee4f36079fa01d159414893beb004e17bbb4
wiebkem <wickoby@gmail.com>
2018-01-25 13:00:43 +0100

    added mail address and linkedin url as text, fixed the menu button

--- js/site.js
+++ js/site.js
@@ -15,6 +15,11 @@
     }
   });
 
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#navigation',
