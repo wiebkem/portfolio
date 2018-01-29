31573de794a9428c8a5f9982156371133a26d714
wiebkem <wickoby@gmail.com>
2018-01-29 21:35:14 +0100

    changed the layout and javascript; added the childrens technology description

--- js/site.js
+++ js/site.js
@@ -20,6 +20,19 @@
     $('.navbar-collapse').collapse('hide');
   });
 
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#navigation").offset().top > 100) {
+      $("#navigation").removeClass("navbar-shrink");
+    } else {
+      $("#navigation").addClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#navigation',
